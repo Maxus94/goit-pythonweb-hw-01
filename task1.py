bf4694ab6f9322f57c16cfbe64912172cd788a37
Maxim <kmaxusk@gmail.com>
2025-03-05 19:26:58 +0200

    task1-completed

--- task1.py
+++ task1.py
@@ -1,26 +1,25 @@
 from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
-    @abstractmethod
-    def start_engine(self):
-        pass
-
-class Car(Vehicle):
+    
     def __init__(self, make, model):
         self.make = make
         self.model = model
-
+    
+    @abstractmethod
+    def start_engine(self):
+        pass
+    
     def __str__(self):
         return f"{self.make} {self.model}"
+
+class Car(Vehicle):    
 
     def start_engine(self):
         print(f"{self.make} {self.model}: Двигун запущено")
 
-class Motorcycle(Vehicle):
-    def __init__(self, make, model):
-        self.make = make
-        self.model = model
-
+class Motorcycle(Vehicle):        
+    
     def start_engine(self):
         print(f"{self.make} {self.model}: Мотор заведено")
 
@@ -38,28 +37,26 @@ class USVehicleFactory(VehicleFactory):
         return Car(make, model), "US_spec"
 
     def create_motorcycle(self, make, model):
-        self.car = Motorcycle(make, model)
-        self.spec = "US_spec"
+        return Motorcycle(make, model), "US_spec"
         
 class EUVehicleFactory(VehicleFactory):
     def create_car(self, make, model):   
         return Car(make, model), "EU_spec" 
 
     def create_motorcycle(self, make, model):
-        self.motorcycle = Motorcycle(make, model)
-        self.spec = "EU_spec"
+        return Motorcycle(make, model), "EU_spec"
 
-
-
-# Використання
 US_factory = USVehicleFactory()
-t = US_factory.create_car("Toyota", "Corolla")
-print(t[0], t[1])
-t[0].start_engine()
+car = US_factory.create_car("Toyota", "Corolla")
+print(car[0], car[1])
+car[0].start_engine()
+moto = US_factory.create_motorcycle("Иж", "Планета")
+print(moto[0], moto[1])
+moto[0].start_engine()
 EU_factory = EUVehicleFactory()
-EU_factory.create_car("Toyota", "Camry")
-# USVehicleFactory.create_car()
-# vehicle1.start_engine()
-
-# vehicle2 = Motorcycle("Harley-Davidson", "Sportster")
-# vehicle2.start_engine()
+car = EU_factory.create_car("Renault", "Duster")
+print(car[0], car[1])
+car[0].start_engine()
+moto = EU_factory.create_motorcycle("Jawa", "350")
+print(moto[0], moto[1])
+moto[0].start_engine()
